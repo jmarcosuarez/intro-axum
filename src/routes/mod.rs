ad811335b9a7a6f3d5590d33a489b373c744cb8d
marcos <jmarcosuarez@gmail.com>
2023-06-02 13:59:32 +0100

    Handle query params

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -2,6 +2,7 @@ mod hello_world;
 mod mirror_body_json;
 mod mirror_body_string;
 mod path_variables;
+mod query_params;
 
 use axum::{
     body::Body,
@@ -12,6 +13,7 @@ use hello_world::hello_world;
 use mirror_body_json::mirror_body_json;
 use mirror_body_string::mirror_body_string;
 use path_variables::{hard_coded_path, path_variables};
+use query_params::query_params;
 
 pub fn create_routes() -> Router<(), Body> {
     Router::new()
@@ -21,5 +23,6 @@ pub fn create_routes() -> Router<(), Body> {
         // start - order is not important. Router will always find the better match
         .route("/path_variables/:id", get(path_variables))
         .route("/path_variables/15", get(hard_coded_path))
-    // end - order is not important. Router will always find the better match
+        // end - order is not important. Router will always find the better match
+        .route("/query_params", get(query_params))
 }
--- src/routes/path_variables.rs
+++ src/routes/path_variables.rs
@@ -1,5 +1,6 @@
 use axum::extract::Path;
 
+// to extract from the path we use the path extractor
 pub async fn path_variables(Path(id): Path<i32>) -> String {
     id.to_string()
 }
