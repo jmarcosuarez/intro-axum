83cdbebd00b929647aa5d14b566e566459ca24db
marcos <jmarcosuarez@gmail.com>
2023-06-02 13:47:56 +0100

    Handle path variables

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,6 +1,7 @@
 mod hello_world;
 mod mirror_body_json;
 mod mirror_body_string;
+mod path_variables;
 
 use axum::{
     body::Body,
@@ -10,10 +11,15 @@ use axum::{
 use hello_world::hello_world;
 use mirror_body_json::mirror_body_json;
 use mirror_body_string::mirror_body_string;
+use path_variables::{hard_coded_path, path_variables};
 
 pub fn create_routes() -> Router<(), Body> {
     Router::new()
         .route("/hello_world", get(hello_world))
         .route("/mirror_body_string", post(mirror_body_string))
         .route("/mirror_body_json", post(mirror_body_json))
+        // start - order is not important. Router will always find the better match
+        .route("/path_variables/:id", get(path_variables))
+        .route("/path_variables/15", get(hard_coded_path))
+    // end - order is not important. Router will always find the better match
 }
