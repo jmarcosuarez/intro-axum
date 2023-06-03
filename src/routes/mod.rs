548e35b554e8a71b52ae34592ca29ae829c835ff
marcos <jmarcosuarez@gmail.com>
2023-06-03 19:54:28 +0100

    Return HTTP status codes

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -8,6 +8,7 @@ mod mirror_user_agent;
 mod path_variables;
 mod query_params;
 mod read_middleware_custom_header;
+mod returns_201;
 mod set_middleware_custom_header;
 
 use always_errors::always_errors;
@@ -27,6 +28,7 @@ use mirror_user_agent::mirror_user_agent;
 use path_variables::{hard_coded_path, path_variables};
 use query_params::query_params;
 use read_middleware_custom_header::read_middleware_custom_header;
+use returns_201::returns_201;
 use set_middleware_custom_header::set_middleware_custom_header;
 
 use tower_http::cors::{Any, CorsLayer};
@@ -65,4 +67,5 @@ pub fn create_routes() -> Router<(), Body> {
         .layer(cors) // this layer will affect all routes above (and not those below)
         .layer(Extension(shared_data)) // use a layer to wrap data and share on other routes using axum::Extension - all above will have access to this data
         .route("/always_errors", get(always_errors))
+        .route("/returns_201", post(returns_201))
 }
