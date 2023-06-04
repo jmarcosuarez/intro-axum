f8fbf21fc59f50bd41e7d40202edba292d965697
marcos <jmarcosuarez@gmail.com>
2023-06-04 23:09:35 +0100

    Add sea-orm and connect it

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 mod routes;
 
 use routes::create_routes;
+use sea_orm::Database;
 
-pub async fn run() {
+pub async fn run(database_uri: &str) {
+    let database = Database::connect(database_uri).await;
     let app = create_routes();
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use dotenvy_macro::dotenv;
 use intro_axum::run;
 
 #[tokio::main]
 async fn main() {
-    run().await
+    // Load environment variables from .env file at compile time
+    let database_uri = dotenv!("DATABASE_URL");
+
+    run(database_uri).await
 }
