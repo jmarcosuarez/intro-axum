d431f057713b7203bde70f5f31fe4093cafb653a
marcos <jmarcosuarez@gmail.com>
2023-06-05 20:47:52 +0100

    Get on eitem from the database

--- src/data_routes/mod.rs
+++ src/data_routes/mod.rs
@@ -6,11 +6,15 @@ use axum::{
 use sea_orm::DatabaseConnection;
 
 mod create_task;
+mod get_one_task;
+
 use create_task::create_task;
+use get_one_task::get_one_task;
 
 pub fn create_routes(database: DatabaseConnection) -> Router<(), Body> {
     Router::new()
         .route("/hello_world", get(|| async { "Hello, World!" }))
         .route("/tasks", post(create_task))
+        .route("/tasks/:task_id", get(get_one_task))
         .layer(Extension(database))
 }
