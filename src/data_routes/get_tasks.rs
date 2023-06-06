d5595934002afa142089ae6a825e626c05009ab8
marcos <jmarcosuarez@gmail.com>
2023-06-06 16:27:07 +0100

    Get all tasks

--- src/data_routes/get_tasks.rs
+++ src/data_routes/get_tasks.rs
@@ -1,5 +1,5 @@
 use crate::database::tasks::Entity as Tasks;
-use axum::{extract::Path, http::StatusCode, response::IntoResponse, Extension, Json};
+use axum::{extract::Path, http::StatusCode, Extension, Json};
 use sea_orm::{DatabaseConnection, EntityTrait};
 use serde::Serialize;
 
@@ -26,4 +26,23 @@ pub async fn get_one_task(
     } else {
         Err(StatusCode::NOT_FOUND)
     }
+}
+
+pub async fn get_all_tasks(
+    Extension(database): Extension<DatabaseConnection>,
+) -> Result<Json<Vec<ResponseTask>>, StatusCode> {
+    let tasks = Tasks::find()
+        .all(&database)
+        .await
+        .map_err(|_error| StatusCode::INTERNAL_SERVER_ERROR)?
+        .into_iter()
+        .map(|db_task| ResponseTask {
+            id: db_task.id,
+            title: db_task.title,
+            priority: db_task.priority,
+            description: db_task.description,
+        })
+        .collect();
+
+    Ok(Json(tasks))
 }
--- src/data_routes/mod.rs
+++ src/data_routes/mod.rs
@@ -6,15 +6,17 @@ use axum::{
 use sea_orm::DatabaseConnection;
 
 mod create_task;
-mod get_one_task;
+mod get_tasks;
 
 use create_task::create_task;
-use get_one_task::get_one_task;
+use get_tasks::get_all_tasks;
+use get_tasks::get_one_task;
 
 pub fn create_routes(database: DatabaseConnection) -> Router<(), Body> {
     Router::new()
         .route("/hello_world", get(|| async { "Hello, World!" }))
         .route("/tasks", post(create_task))
+        .route("/tasks", get(get_all_tasks))
         .route("/tasks/:task_id", get(get_one_task))
         .layer(Extension(database))
 }
