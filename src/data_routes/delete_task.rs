3ec9fd9f72ba4b4c3aecd3a11b3cd7eb17e82680
marcos <jmarcosuarez@gmail.com>
2023-06-07 21:55:28 +0100

    Add soft delete task records

--- src/data_routes/delete_task.rs
+++ src/data_routes/delete_task.rs
@@ -1,10 +1,21 @@
-use crate::database::tasks::{self, Entity as Tasks};
-use axum::{extract::Path, http::StatusCode, Extension};
-use sea_orm::{ColumnTrait, DatabaseConnection, EntityTrait, IntoActiveModel, QueryFilter};
+use crate::database::tasks::Entity as Tasks;
+use axum::{
+    extract::{Path, Query},
+    http::StatusCode,
+    Extension,
+};
+use sea_orm::{sea_query::tests_cfg::Task, DatabaseConnection, EntityTrait, IntoActiveModel, Set};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct QueryParams {
+    soft: bool,
+}
 
 pub async fn delete_task(
     Path(task_id): Path<i32>,
     Extension(database): Extension<DatabaseConnection>,
+    Query(query_params): Query<QueryParams>,
 ) -> Result<(), StatusCode> {
     // 1. This is one way - return 404 if not found as well
 
@@ -24,13 +35,29 @@ pub async fn delete_task(
     //     .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
 
     // Ok(())
-
-    //  2. with 200 OK regardless - instead of 404
-    Tasks::delete_by_id(task_id)
-        .exec(&database)
-        .await
-        .map_err(|_err| StatusCode::INTERNAL_SERVER_ERROR)?;
-
+    if (query_params.soft) {
+        let mut task = if let Some(task) = Tasks::find_by_id(task_id)
+            .one(&database)
+            .await
+            .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?
+        {
+            task.into_active_model()
+        } else {
+            return Err(StatusCode::NOT_FOUND);
+        };
+        let now = chrono::Utc::now();
+        task.deleted_at = Set(Some(now.into()));
+        Tasks::update(task)
+            .exec(&database)
+            .await
+            .map_err(|_err| StatusCode::INTERNAL_SERVER_ERROR)?;
+    } else {
+        //  2. with 200 OK regardless - instead of 404
+        Tasks::delete_by_id(task_id)
+            .exec(&database)
+            .await
+            .map_err(|_err| StatusCode::INTERNAL_SERVER_ERROR)?;
+    }
     // 3. Delete using delete_many()
     // Tasks::delete_many()
     //     .filter(tasks::Column::Id.eq(task_id))
