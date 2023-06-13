0b5cd6904249565f4a5243fb3b2e931998635b36
marcos <jmarcosuarez@gmail.com>
2023-06-13 20:45:18 +0100

    Hash password

--- src/data_routes/user.rs
+++ src/data_routes/user.rs
@@ -27,7 +27,7 @@ pub async fn create_user(
 ) -> Result<Json<ResponseUser>, StatusCode> {
     let new_user = users::ActiveModel {
         username: Set(request_user.username),
-        password: Set(request_user.password), // to be removed to a hashed password
+        password: Set(hash_password(request_user.password)?), // to be removed to a hashed password
         token: Set(Some("jfu4hf48u4839ir9m43u5943m5i943i59435".to_owned())),
         ..Default::default()
     }
@@ -53,6 +53,10 @@ pub async fn login(
         .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
 
     if let Some(db_user) = db_user {
+        if !verify_password(request_user.password, &db_user.password)? {
+            return Err(StatusCode::UNAUTHORIZED);
+        }
+
         // do the login
         let new_token = "d2s8fjs9dj2s2d98sjd9sd9sds".to_owned();
         let mut user = db_user.into_active_model();
@@ -86,4 +90,12 @@ pub async fn logout(
         .map_err(|_err| StatusCode::INTERNAL_SERVER_ERROR)?;
 
     Ok(())
+}
+
+fn hash_password(password: String) -> Result<String, StatusCode> {
+    bcrypt::hash(password, 14).map_err(|_error| StatusCode::INTERNAL_SERVER_ERROR)
+}
+
+fn verify_password(password: String, hash: &str) -> Result<bool, StatusCode> {
+    bcrypt::verify(password, hash).map_err(|_error| StatusCode::INTERNAL_SERVER_ERROR)
 }
